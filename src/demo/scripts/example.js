fbb1bf72e42da53a38db47914eed15239f6e81c1
Essential Randomness <essentialrandomn3ss@gmail.com>
2020-03-26 01:35:47 -0700

    add drag and drop and copy paste

--- src/demo/scripts/example.js
+++ src/demo/scripts/example.js
@@ -10,6 +10,7 @@ const context = canvas.getContext("2d");
 const canvasContainer = document.querySelector(".canvasContainer");
 const filterToImg = img => {
   canvasContainer.classList.add("loading");
+  canvasContainer.classList.remove("empty");
   canvas.width = img.naturalWidth;
   canvas.height = img.naturalHeight;
   context.drawImage(img, 0, 0);
@@ -43,7 +44,7 @@ urlSelector.addEventListener("click", () => {
   };
   img.onerror = () => {
     alert(
-      "Failed to load the image. This is most likely due to limitations of the server it's hosted on. Sorry! Try uploading it from disk instead."
+      "Failed to load the image. This is most likely due to limitations of the server it's hosted on. Sorry! Hint: try copy/pasting instead."
     );
   };
   img.src = url;
@@ -68,4 +69,91 @@ fileInput.addEventListener("change", e => {
     newImage.src = fileLoadedEvent.target.result;
   };
   fileReader.readAsDataURL(filesSelected[0]);
+});
+
+document.body.addEventListener(
+  "dragstart",
+  () => {
+    canvasContainer.classList.add("dragging");
+  },
+  false
+);
+document.body.addEventListener(
+  "dragend",
+  () => canvasContainer.classList.remove("dragging"),
+  false
+);
+
+canvasContainer.addEventListener(
+  "dragenter",
+  () => canvasContainer.classList.add("enter"),
+  false
+);
+canvasContainer.addEventListener(
+  "dragleave",
+  () => {
+    canvasContainer.classList.remove("enter");
+  },
+  false
+);
+canvasContainer.addEventListener(
+  "dragover",
+  e => {
+    e.preventDefault();
+    e.stopPropagation();
+  },
+  false
+);
+
+canvasContainer.addEventListener(
+  "drop",
+  e => {
+    console.log(e);
+    console.log(e.dataTransfer.files);
+    /*for (let i = 0; i < e.dataTransfer.items.length; i++) {
+      console.log(e.dataTransfer.items[i]);
+    }*/
+
+    console.log(e.dataTransfer.getData("text/html"));
+    e.preventDefault();
+    canvasContainer.classList.remove("enter");
+    canvasContainer.classList.remove("dragging");
+
+    if (e.dataTransfer.files.length > 0) {
+      const fileReader = new FileReader();
+
+      fileReader.onload = function(fileLoadedEvent) {
+        const newImage = document.createElement("img");
+        newImage.onload = () => filterToImg(newImage);
+        newImage.src = fileLoadedEvent.target.result;
+      };
+      fileReader.readAsDataURL(e.dataTransfer.files[0]);
+    }
+  },
+  false
+);
+
+const imageTypes = ["image/png", "image/gif", "image/bmp", "image/jpg"];
+document.addEventListener("paste", e => {
+  console.log(e);
+  console.log(e.clipboardData.items);
+  console.log(JSON.stringify(e.clipboardData.items));
+  console.log("paste action initiated");
+
+  const items = e.clipboardData.items;
+
+  for (let i = 0; i < items.length; i++) {
+    if (imageTypes.includes(items[i].type)) {
+      const fileReader = new FileReader();
+
+      fileReader.onload = function(fileLoadedEvent) {
+        const newImage = document.createElement("img");
+        newImage.onload = () => filterToImg(newImage);
+        newImage.src = fileLoadedEvent.target.result;
+      };
+      fileReader.readAsDataURL(items[i].getAsFile());
+    }
+  }
+
+  e.preventDefault();
 });
