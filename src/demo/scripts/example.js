37c239efb371c3c94de16617d6398088d6631265
Essential Randomness <essentialrandomn3ss@gmail.com>
2020-03-25 17:14:24 -0700

    add loading overlay

--- src/demo/scripts/example.js
+++ src/demo/scripts/example.js
@@ -7,12 +7,14 @@ const IMAGES = importAll(require.context("../images/", true, /\.jpg$/));
 const canvas = document.querySelector("#canvas");
 const context = canvas.getContext("2d");
 
+const canvasContainer = document.querySelector(".canvasContainer");
 const filterToImg = e => {
-  console.log(e.target, canvas);
+  canvasContainer.classList.add("loading");
   canvas.width = e.target.naturalWidth;
   canvas.height = e.target.naturalHeight;
   context.drawImage(e.target, 0, 0);
   applyFilter(canvas, mishaFilter).then(result => {
+    canvasContainer.classList.remove("loading");
     console.log("TADAAA!!");
   });
 };
