22f937ec3796c4979e8eeeb065b45b5b9eec4bf7
Essential Randomness <essentialrandomn3ss@gmail.com>
2020-03-25 17:14:15 -0700

    keep misha ratio

--- src/misha-filter.ts
+++ src/misha-filter.ts
@@ -6,6 +6,17 @@ function importAll(r: __WebpackModuleApi.RequireContext) {
   return r.keys().map(moduleName => r(moduleName).default);
 }
 
+function calculateAspectRatioFit(
+  srcWidth: number,
+  srcHeight: number,
+  maxWidth: number,
+  maxHeight: number
+): { width: number; height: number } {
+  var ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
+
+  return { width: srcWidth * ratio, height: srcHeight * ratio };
+}
+
 const MISHAS = importAll(require.context("./mishaPics/", true, /\.png$/));
 const IMAGES: { [key: string]: HTMLImageElement } = {};
 
@@ -37,15 +48,33 @@ export default async (
 
   // Add some margin so the face is slightly bigger than the target.
   // The margin will depend on the box size. Let's say 1/5.
-  const marginX = faceBox.width / 5;
-  const marginY = faceBox.height / 5;
+  const boxMarginX = faceBox.width / 5;
+  const boxMarginY = faceBox.height / 5;
+  const newX = faceBox.x - boxMarginX;
+  const newY = faceBox.y - boxMarginY;
+  const newWidth = faceBox.width + 2 * boxMarginX;
+  const newHeight = faceBox.height + 2 * boxMarginY;
+
+  // get aspect ratio of Misha within new box boundaries
+  const finalRatio = calculateAspectRatioFit(
+    misha.naturalWidth,
+    misha.naturalHeight,
+    newWidth,
+    newHeight
+  );
+
+  // Figure out which offset we need so Misha is centered
+  // within the new boxes boundaries.
+  const offsetX = (newWidth - finalRatio.width) / 2;
+  const offsetY = (newHeight - finalRatio.height) / 2;
+
   const context = target.getContext("2d");
   context?.drawImage(
     misha,
-    faceBox.x - marginX,
-    faceBox.y - marginY,
-    faceBox.width + 2 * marginX,
-    faceBox.height + 2 * marginY
+    newX + offsetX,
+    newY + offsetY,
+    finalRatio.width,
+    finalRatio.height
   );
 
   return target;
