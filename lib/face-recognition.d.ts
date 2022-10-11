fff3042304b05324313be953571e8d27b41e4a05
Essential Randomness <essential.randomn3ss@gmail.com>
2022-10-10 17:26:23 -0700

    fix fix fix

--- lib/face-recognition.d.ts
+++ lib/face-recognition.d.ts
@@ -1,3 +1,3 @@
-import * as faceapi from "face-api.js";
 import "regenerator-runtime";
+import * as faceapi from "face-api.js";
 export declare const findFace: (input: faceapi.TNetInput) => Promise<faceapi.Box<any>[]>;
